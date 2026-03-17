091271551258a7e11fb77efe9131e673ce36a2ae
Azure2212 <Dasanbob22122002@gmail.com>
2026-03-17 01:48:13 -0400

    update

--- models/efficientNetV2.py
+++ models/efficientNetV2.py
@@ -14,10 +14,10 @@ def load_EfficientNetV2(num_classes, type_size = "s", pretrained_path="", device
             num_classes=num_classes, drop_rate=0.2, drop_path_rate=0.3)
         print("Loading EfficientNetV2_m model successfully!\n")
     elif type_size == "l":
-    # EfficientNetV2-L
+        # EfficientNetV2-L
         model = timm.create_model("tf_efficientnetv2_l", pretrained=True, 
             num_classes=num_classes, drop_rate=0.3, drop_path_rate=0.4)
-    print("Loading EfficientNetV2_l model successfully!\n")
+        print("Loading EfficientNetV2_l model successfully!\n")
     if pretrained_path != "":
         state = torch.load(pretrained_path, map_location=device)
         model.load_state_dict(state["net"])
